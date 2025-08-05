172d9845a8fd20942848a7118991eb61df956b74
tahsiangyang <leo.yang@zealogics.com>
2025-08-06 03:52:15 +0800

    remove compile generated files.

--- CM7/Debug/Core/Src/arm_cfft_f32.d
+++
@@ -1,9 +0,0 @@
-Core/Src/arm_cfft_f32.o: ../Core/Src/arm_cfft_f32.c \
- ../Core/Inc/arm_math.h ../../Drivers/CMSIS/Include/cmsis_compiler.h \
- ../../Drivers/CMSIS/Include/cmsis_gcc.h ../Core/Inc/arm_common_tables.h \
- ../Core/Inc/arm_math.h
-../Core/Inc/arm_math.h:
-../../Drivers/CMSIS/Include/cmsis_compiler.h:
-../../Drivers/CMSIS/Include/cmsis_gcc.h:
-../Core/Inc/arm_common_tables.h:
-../Core/Inc/arm_math.h:
--- CM7/Debug/Core/Src/arm_fft_bin_data.d
+++
@@ -1,6 +0,0 @@
-Core/Src/arm_fft_bin_data.o: ../Core/Src/arm_fft_bin_data.c \
- ../Core/Inc/arm_math.h ../../Drivers/CMSIS/Include/cmsis_compiler.h \
- ../../Drivers/CMSIS/Include/cmsis_gcc.h
-../Core/Inc/arm_math.h:
-../../Drivers/CMSIS/Include/cmsis_compiler.h:
-../../Drivers/CMSIS/Include/cmsis_gcc.h:
